a2eb81f34193876e9359f089deaeb07d0039f706
Suryansee Singh Parihar <suryansees@gmail.com>
2025-05-14 12:35:00 +0000

    done indian standards

--- 01-basics/06_NumberMaths.js
+++ 01-basics/06_NumberMaths.js
@@ -3,3 +3,20 @@ console.log(typeof score);
 
 const balance=new Number(100);
 console.log(balance);
+console.log(balance.toString());
+console.log(typeof balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));// used to control the pricision value
+const otherNumber=12.8966;
+console.log(otherNumber.toPrecision(3));
+const otherNumber2=212.8966;
+console.log(otherNumber2.toPrecision(3));
+const otherNumber3=3212.8966;
+console.log(otherNumber3.toPrecision(3));// Returns a string containing a number represented either in exponential
+//  or fixed-point notation with a specified number of digits.
+const hundreds=100000000;
+console.log(hundreds.toLocaleString());//output--- 100,000,000  separated with comma  [Converts a number to a string by using the current or specified locale].
+console.log(hundreds.toLocaleString()('en-IN'));
+
+
+
