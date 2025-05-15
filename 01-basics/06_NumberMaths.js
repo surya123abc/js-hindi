326c65bff0d44f705346e613bba366894d5229c5
Suryansee Singh Parihar <suryansees@gmail.com>
2025-05-15 02:07:11 +0000

    understand date in js

--- 01-basics/06_NumberMaths.js
+++ 01-basics/06_NumberMaths.js
@@ -1,31 +1,38 @@
-const score=400;
-console.log(typeof score);
+//const score=400;
+//console.log(typeof score);
 
-const balance=new Number(100);
-console.log(balance);
-console.log(balance.toString());
-console.log(typeof balance);
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));// used to control the pricision value
-const otherNumber=12.8966;
-console.log(otherNumber.toPrecision(3));
-const otherNumber2=212.8966;
-console.log(otherNumber2.toPrecision(3));
-const otherNumber3=3212.8966;
-console.log(otherNumber3.toPrecision(3));// Returns a string containing a number represented either in exponential
+//const balance=new Number(100);
+//console.log(balance);
+//console.log(balance.toString());
+//console.log(typeof balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));// used to control the pricision value
+//const otherNumber=12.8966;
+//console.log(otherNumber.toPrecision(3));
+//const otherNumber2=212.8966;
+//console.log(otherNumber2.toPrecision(3));
+//const otherNumber3=3212.8966;
+//console.log(otherNumber3.toPrecision(3));// Returns a string containing a number represented either in exponential
 //  or fixed-point notation with a specified number of digits.
-const hundreds=100000000;
-console.log(hundreds.toLocaleString());//output--- 100,000,000  separated with comma  [Converts a number to a string by using the current or specified locale].
-console.log(hundreds.toLocaleString('en-IN'));
+//const hundreds=100000000;
+//console.log(hundreds.toLocaleString());//output--- 100,000,000  separated with comma  [Converts a number to a string by using the current or specified locale].
+//console.log(hundreds.toLocaleString('en-IN'));
 
 
 //+++++++++++++++++++++ Maths+++++++++++
 
-console.log(Maths);
-console.log(Maths.min(4));
-console.log(Maths.abs(-4));
-console.log(Math.ceil(4.2));
-console.log(Math.floor(4.9));
-console.log(Math.round(4.6));
+//console.log(Math);
+//console.log(Math.min(4,3,9,8));
+//console.log(Math.abs(-4));
+//console.log(Math.ceil(4.2));
+//console.log(Math.floor(4.9));
+//console.log(Math.round(4.6));
+//console.log(Math.max(4,3,9,8));
+console.log(Math.random());//      gives values b/t 0 and 1
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
 
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
 
