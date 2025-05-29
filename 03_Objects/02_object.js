3185613e862e4b58a461ec745fcc886a946cd590
Suryansee Singh Parihar <suryansees@gmail.com>
2025-05-29 12:44:17 +0000

    destructure and jason

--- 03_Objects/02_object.js
+++ 03_Objects/02_object.js
@@ -20,6 +20,27 @@ const regularUser={
 //console.log(regularUser.fullName.halfFullName?.userFullName);// this is called optional chaining,used instead of unexisting value that is not present
 const obj1={1:"a",2:"b"};
 const obj2={3:"a",4:"b"};
-const obj3={obj1,obj2};
+//const obj3={obj1,obj2};
+//console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+//const obj3=Object.assign({},obj1,obj2);
+//console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+const obj3={...obj1, ...obj2};
+console.log(obj3);
+const user=[
+    {
+
+    },
+    {
+
+    },
+    {
+        id:"1",
+       email:"suryansees@google.com"
+    }
+]
+//console.log(user[1].email);
+//console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.entries);
 
 
---
+++ 03_Objects/03_DestructureObj.js
@@ -0,0 +1,33 @@
+// destructure of object
+
+const course={
+
+        courseName:"js in hindi",
+        courseInstructor:"Suryansee",
+        price:999
+
+}
+//course.courseInstructor
+
+//another method
+//const{courseInstructor:Instructor}=course
+//console.log(courseInstructor);
+//console.log(Instructor);
+
+
+// JASON API
+//{
+        //"courseName":js in hindi",
+       // "courseInstructor":"Suryansee",
+        //"price":"free"
+//}
+
+//in Array form
+//[
+    // {}
+    // {}
+    // {}
+
+    /// RandomUserMe API
+    
+//]
